1f7e52cd4054465ea71865c0dace60ed97f01766
Luis Mora <luismora371@gmail.com>
2022-01-24 11:34:02 -0600

    added update recipe route

--- src/routers/recipe.router.js
+++ src/routers/recipe.router.js
@@ -5,5 +5,5 @@ const controller = require('../controllers/recipe.controller')
 const router = express.Router()
 
 router.post('/',controller.createRecipe)
-
+router.patch('/update-recipe',controller.updateRecipe)
 module.exports = router
