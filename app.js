8fe92603b1f6c490a3c8c35fe87472d785d9a41a
Rafael Martinez <rterranova701@gmail.com>
2022-01-28 08:55:12 -0600

    Add env variables for stripe

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.API_KEY_PAYMENTS);
 // price_1KMXZcArpXm7B0HUJcKKVElx
 const dbConnect = require('./src/lib/db')
+const port = process.env.PORT || '8080'
 
 const server = require('./src/lib/server')
 
 dbConnect(process.env)
     .then(()=>{
         console.log('DB Connected!');
-        server.listen(process.env.PORT,()=>{
-            console.log('Server is running');
+        server.listen(port,()=>{
+            console.log('Server is running on port: ' + port);
         })
     })
     .catch((error)=>{
