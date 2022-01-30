e3d5651aeff160dcb39b4e433577d528f8e9fec4
Luis Mora <luismora371@gmail.com>
2022-01-30 14:45:18 -0600

    Added API model

--- src/models/api.model.js
+++ src/models/api.model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const apiSchema = new mongoose.Schema({
-
+    socialHandles: {
+        type: Map, // `socialHandles` is a key/value store for string keys
+        of: String // Values must be strings
+        }
 })
 
-const ApiKeys = mongoose.model('user', apiSchema);
+const ApiKey = mongoose.model('ApiKey', apiSchema);
 
-module.exports = ApiKeys;
+module.exports = ApiKey;
