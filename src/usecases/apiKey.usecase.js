02023a24a0158bb783c9c329c1f0b34e1a338293
Luis Mora <luismora371@gmail.com>
2022-01-30 14:46:27 -0600

    added CRUD usecases to payment

---
+++ src/usecases/apiKey.usecase.js
@@ -0,0 +1,58 @@
+const ApiKey = require ('../models/api.model')
+
+
+async function saveApiKey(credential) {
+    // return ApiKey.set(credential.apikey, credential.customerID)
+    const newkey = new ApiKey({
+      socialHandles: {
+        [credential.apikey]: credential.customerID,
+      }
+    })
+    newkey.save(function(err, result){
+      if(err){
+          console.log('>>>>>> Error');
+          console.log(err)
+          return err
+      }else{
+          console.log('>>>>>> ' + JSON.stringify(result, null, 4));
+          return result
+      }
+  });
+    // return await ApiKey.socialHandles.set('credential.apikey', 'credential.customerID');
+  }
+
+async function getKeys(){
+  return await ApiKey.find();
+}
+
+
+async function getCustomerID(apikey){
+  // console.log(ApiKey.findOne({ 'socialHandles.github': 'vkarpov15' }))
+  const apiKeys = await getKeys()
+  let res = {};
+  apiKeys.forEach((item)=>{
+    const key = new ApiKey(item)
+    if(key.socialHandles.get(apikey)){
+      console.log(item)
+      res = key.socialHandles.get(apikey)
+      console.log(key.socialHandles.get(apikey))
+    }
+  })
+  return res
+}
+
+async function getApiKey(){
+  return 'Working feature'
+}
+
+async function deleteApiKey(id){
+  return await ApiKey.findByIdAndRemove(id)
+}
+
+module.exports = {
+  saveApiKey,
+  getKeys,
+  getCustomerID,
+  getCustomerID,
+  deleteApiKey
+}
