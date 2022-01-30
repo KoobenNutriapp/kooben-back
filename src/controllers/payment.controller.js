6fcf3edc40b9bd318ec798ffd43c3712b2920e03
Luis Mora <luismora371@gmail.com>
2022-01-30 14:45:07 -0600

    added CRUD controllers to payment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const stripe = require('stripe')(process.env.API_KEY_PAYMENTS);
+const user = require("../usecases/user.usecase");
+const stripeKey = require("../usecases/apiKey.usecase");
 
 ////// Data Model ///////
 
@@ -32,7 +34,7 @@ async function checkout(request,response){
             // the actual Session ID is returned in the query parameter when your customer
             // is redirected to the success page.
             success_url:
-              'http://localhost:8080/?session_id={CHECKOUT_SESSION_ID}',
+              'https://www.youtube.com/?session_id={CHECKOUT_SESSION_ID}',
             cancel_url: 'http://localhost:8080/error',
           });
         
@@ -76,44 +78,42 @@ function hashAPIKey(apiKey) {
     return hashedAPIKey;
   }
 
-async function webhook(request,response){
+async function webhook(req,res){
     try {
-        let data;
-        let eventType;
-        // Check if webhook signing is configured.
-        const webhookSecret = process.env.WEBHOOK_SECRET;
-
-        if (webhookSecret) {
-            // Retrieve the event by verifying the signature using the raw body and secret.
-            let event;
-            let signature = req.headers['stripe-signature'];
-
-            try {
-            event = stripe.webhooks.constructEvent(
-                req['rawBody'],
-                signature,
-                webhookSecret
-            );
-            } catch (err) {
-            console.log(`⚠️  Webhook signature verification failed.`);
-            return res.sendStatus(400);
-            }
-            // Extract the object from the event.
-            data = event.data;
-            eventType = event.type;
-        } else {
-            // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-            // retrieve the event data directly from the request body.
-            data = req.body.data;
-            eventType = req.body.type;
+        console.log('****** WEB HOOK CONTROLLER *********')
+        const payload = req.body
+        // const sig = req.headers['stripe-signature']
+        const payloadString = JSON.stringify(payload, null, 2);
+        const secret = process.env.WEBHOOK_SECRET;
+        const header = stripe.webhooks.generateTestHeaderString({
+                payload: payloadString,
+                secret,
+        });
+        
+        let event;
+        try {
+            event = stripe.webhooks.constructEvent(payloadString, header, secret);
+        
+        } catch (err) {
+                console.log(`Webhook Error: ${err.message}`)
+                console.log(err)
+                return res.status(400).send(`Webhook Error: ${err.message}`);
         }
 
-        switch (eventType) {
-            case 'checkout.session.completed':
-            console.log(data);
+    // Handle the event
+    let newUser = {}
+    switch (event.type) {
+        case 'customer.subscription.created':
+            console.log('-------customer.subscription.created-------');
+            console.log(event.data)
+
+        break;
+        case 'checkout.session.completed':
+            console.log('---------checkout.session.completed------------');
+            console.log(event);
             // Data included in the event object:
-            const customerId = data.object.customer;
-            const subscriptionId = data.object.subscription;
+            const customerId = event.data.object.customer;
+            const subscriptionId = event.data.object.subscription;
 
             console.log(
                 `💰 Customer ${customerId} subscribed to plan ${subscriptionId}`
@@ -124,6 +124,7 @@ async function webhook(request,response){
             const itemId = subscription.items.data[0].id;
 
             // Generate API key
+            console.log('GENERATE KEYS')
             const { apiKey, hashedAPIKey } = generateAPIKey();
             console.log(`User's API Key: ${apiKey}`);
             console.log(`Hashed API Key: ${hashedAPIKey}`);
@@ -136,27 +137,207 @@ async function webhook(request,response){
             };
             apiKeys[hashedAPIKey] = customerId;
 
-            break;
-            case 'invoice.paid':
-            // Continue to provision the subscription as payments continue to be made.
-            // Store the status in your database and check when a user accesses your service.
-            // This approach helps you avoid hitting rate limits.
-            break;
-            case 'invoice.payment_failed':
-            // The payment failed or the customer does not have a valid payment method.
-            // The subscription becomes past_due. Notify your customer and send them to the
-            // customer portal to update their payment information.
-            break;
-            default:
-            // Unhandled event type
-        }
+            try {
+                newUser = {
+                    'name': 'toChange',
+                    'status':true,
+                    'mail': 'someMail',
+                    'recipes': [''],
+                    'likes': [''],
+                    'rol': 'somRol',
+                    'type': 'ApiUser',
+                    'stripeCustomer':{
+                        'id': customerId,
+                        'active': true,
+                        apiKey: hashedAPIKey,
+                        itemId
+                    },
+                }
+                const createdUser = await user.createUser(newUser)
+                console.log('---USER CREATED------');
+                console.log(createdUser)
+                const createApiKeys = await stripeKey.saveApiKey({'apikey':createdUser.stripeCustomer.apiKey,'customerID':createdUser._id});
+                console.log(createApiKeys)
+            } catch (error) {
+                console.error(error);
+                res.statusCode = 500;
+                res.json({
+                success: false,
+                message: "Could not create substription",
+                error,
+                });
+            }
 
-  res.sendStatus(200);
+        break;
+        case 'invoice.paid':
+        // Continue to provision the subscription as payments continue to be made.
+        // Store the status in your database and check when a user accesses your service.
+        // This approach helps you avoid hitting rate limits.
+        break;
+        case 'invoice.payment_failed':
+        // The payment failed or the customer does not have a valid payment method.
+        // The subscription becomes past_due. Notify your customer and send them to the
+        // customer portal to update their payment information.
+        break;
+        case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        console.log('--------payment_intent.succeeded-------');
+        console.log('Payment succeded')
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+        case 'invoice.created':
+            console.log('--------invoice created-------');
+            console.log(event.data)
+            break;
+        case 'customer.updated':
+            console.log('--------customer.updated-------');
+            console.log(event.data)
+            break;
+        case 'invoice.finalized':
+            console.log('--------invoice.finalized-------');
+            console.log(event.data)
+            break;
+        case 'customer.deleted':
+            console.log('--------customer.deleted-------');
+            console.log(event.data)
+            break;
+        case 'invoice.payment_succeeded':
+            console.log('--------customer.deleted-------');
+            console.log(event.data)
+            break;
+        default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+        // Return a 200 response to acknowledge receipt of the event
+        res.sendStatus(200);
     } catch (error) {
-        
+        console.log(error)
     }
 }
+
+async function addKey(req,res){
+    try{
+        console.log(req.body)
+        const createApiKeys = await stripeKey.saveApiKey({'apikey': req.body.apiKey,'customerID':req.body._id});
+        console.log(createApiKeys)
+        res.statusCode = 201
+        res.json({
+            success: true,
+            message: 'AppiKey succesfully Added!',
+            data: {
+                createApiKeys
+            }
+        })
+    }catch (error) {
+        console.error(error);
+        res.statusCode = 500;
+        res.json({
+        success: false,
+        message: "Could not add API",
+        error,
+        });
+    }
+
+}
+
+
+async function getKeys(req,res){
+    try{
+        const allkeys = await stripeKey.getKeys();
+        res.statusCode = 201
+        res.json({
+            success: true,
+            message: 'Keys!',
+            data: {
+                allkeys
+            }
+        })
+    }catch(err){
+        console.error(error);
+        res.statusCode = 500;
+        res.json({
+        success: false,
+        message: "Could not get ApiKeys",
+        error,
+        });
+    }
+}
+
+async function getCustomerID(req,res){
+    const id = req.query.apiKey;
+    console.log("id: ", id)
+    try{
+        const customerId = await stripeKey.getCustomerID(id);
+        res.statusCode = 201
+        res.json({
+            success: true,
+            message: 'CostumerID!',
+            data: {
+                customerId
+            }
+        })
+    }catch(err){
+        console.error(err);
+        res.statusCode = 500;
+        res.json({
+        success: false,
+        message: "Could not get CustomerID",
+        err,
+        });
+    }
+}
+
+async function getApiKey(req,res){
+    const id = req.params.customerId;
+    try{
+        const allkeys = await stripeKey.getApiKey(id);
+        res.statusCode = 201
+        res.json({
+            success: true,
+            message: 'Keys!',
+            data: {
+                allkeys
+            }
+        })
+    }catch(err){
+        console.error(error);
+        res.statusCode = 500;
+        res.json({
+        success: false,
+        message: "Could not get ApiKey",
+        error,
+        });
+    }
+}
+
+async function deleteApiKey(request,response){
+    const ApiKeyId = request.params.id;
+  try {
+    await stripeKey.deleteApiKey(ApiKeyId);
+
+    response.statusCode = 200;
+    response.json({
+      success: true,
+      message: "ApiKey succesfully deleted!",
+    });
+  } catch (error) {
+    console.error(error);
+    response.statusCode = 500;
+    response.json({
+      success: false,
+      message: "Could not delete ApiKey",
+      error,
+    });
+  }
+}
+
 module.exports = {
     checkout,
-    webhook
+    webhook,
+    addKey,
+    getKeys,
+    getCustomerID,
+    getApiKey,
+    deleteApiKey
 }
