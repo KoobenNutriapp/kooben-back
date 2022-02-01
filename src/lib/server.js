62f9b908b60a692ff1104cfc017b1f63df7bdf6e
Luis Mora <luismora371@gmail.com>
2022-02-01 15:40:46 -0600

    updated servers

--- src/lib/server.js
+++ src/lib/server.js
@@ -4,8 +4,10 @@ const ingredient = require('../routers/ingredient.router')
 const payment = require('../routers/payment.router')
 const logger = require('../middlewares/logger')
 const user = require('../routers/user.router')
+const User = require('../usecases/user.usecase')
 const stripe = require('stripe')(process.env.API_KEY_PAYMENTS);
 const bodyParser = require('body-parser');
+const stripeKey = require("../usecases/apiKey.usecase");
 //const cors = require('cors')
 const server = express()
 
@@ -18,33 +20,9 @@ const errorController = require('../controllers/error');
 server.use(express.json())
 server.use(logger)
 
-
-//Routes
-server.get('/',(req,res)=>res.send('Bienvenido a Kooben Rafa,Balan, Mora, Paco'))
-server.use('/recipe',recipe)
-server.use('/ingredient',ingredient)
-server.use('/payment',payment)
-server.use('/user',user)
-
 // 🔥🔥🔥🔥🔥🔥🔥🔥 API 🔥🔥🔥🔥🔥🔥🔥🔥
-////// Data Model ///////
 
-// TODO Implement a real database
-// Reverse mservering of stripe to API key. Model this in your preferred database.
-const customers = {
-    // stripeCustomerId : data
-    'stripeCustomerId': {
-      apiKey: '123xyz',
-      active: false,
-      itemId: 'stripeSubscriptionItemId',
-    },
-  };
-  const apiKeys = {
-    // apiKey : customerdata
-    '123xyz': 'stripeCustomerId',
-  };
-  
-  ////// Custom API Key Generation & Hashing ///////
+ ////// Custom API Key Generation & Hashing ///////
   
   // Recursive function to generate a unique random string as API key
   function generateAPIKey() {
@@ -68,63 +46,65 @@ const customers = {
   
     return hashedAPIKey;
   }
-  
-  ////// Express API ///////
-  
-  
-  // Get information about the customer
-  server.get('/customers/:id', (req, res) => {
-    const customerId = req.params.id;
-    const account = customers[customerId];
-    if (account) {
-      res.send(account);
-    } else {
-      res.sendStatus(404);
-    }
-  });
-  
-  // Make a call to the API
-  server.get('/api', async (req, res) => {
-    const { apiKey } = req.query;
-    // const apiKey = req.headers['X-API-KEY'] // better option for storing API keys
-  
+
+// Register for API
+server.use('/stripe',payment)
+
+// Monitor & API usage -- COMENT FUNCTION FOR DEVELOPMENT
+server.all('*', async (req, res, next) => {
+    // const { apiKey } = req.query;
+    console.log('Validacion de token')
+    console.log(req.headers)
+    const apiKey = req.headers['X-API-KEY'] ? req.headers['X-API-KEY'] : req.headers['x-api-key'] // better option for storing API keys
     if (!apiKey) {
-      res.sendStatus(400); // bad request
+        res.statusCode = 400
+        res.json({
+            success: false,
+            message: 'Missing api key'
+        })
     }
-  
+    // check if accouunt is active
     const hashedAPIKey = hashAPIKey(apiKey);
-  
-    const customerId = apiKeys[hashedAPIKey];
-    const customer = customers[customerId];
-    
-  
-    if (!customer || !customer.active) {
+    const customerId = await stripeKey.getCustomerID(hashedAPIKey)
+
+    if (!customerId) {
       res.sendStatus(403); // not authorized
     } else {
-  
-      // Record usage with Stripe Billing
-      const record = await stripe.subscriptionItems.createUsageRecord(
-        customer.itemId,
+    
+        // get user
+        const customer = await User.getUserById(customerId)
+        console.log(customer)
+        // Record usage with Stripe Billing
+        const record = await stripe.subscriptionItems.createUsageRecord(
+        customer.stripeCustomer.itemId,
         {
-          quantity: 1,
-          timestamp: 'now',
-          action: 'increment',
+            quantity: 1,
+            timestamp: 'now',
+            action: 'increment',
         }
-      );
-      res.send({ data: '🔥🔥🔥🔥🔥🔥🔥🔥', usage: record });
+        );
+
+    next();
     }
   });
-  
-  server.get('/usage/:customer', async (req, res) => {
-    const customerId = req.params.customer;
-    const invoice = await stripe.invoices.retrieveUpcoming({
-      customer: customerId,
-    });
-  
-    res.send(invoice);
-  });
 
-// 🔥🔥🔥🔥🔥🔥🔥🔥 API 🔥🔥🔥🔥🔥🔥🔥🔥
+//Routes
+server.get('/',(req,res)=>res.send('Bienvenido a Kooben Rafa,Balan, Mora, Paco'))
+server.use('/recipe',recipe)
+server.use('/ingredient',ingredient)
+server.use('/payment',payment)
+server.use('/user',user)  
+
+// Customer API usage report  
+server.get('/usage/:customer', async (req, res) => {
+const customerId = req.params.customer;
+const invoice = await stripe.invoices.retrieveUpcoming({
+    customer: customerId,
+});
+
+res.send(invoice);
+});
+
 // 404 - NOT- FOUND
 server.use(errorController.get404);
 
