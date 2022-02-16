757d1cd060eb273dc11bc7dec59d9ef7e4554564
Rafael Martinez <rterranova701@gmail.com>
2022-02-16 16:37:21 -0600

    Fix recipe model

--- src/models/recipe.model.js
+++ src/models/recipe.model.js
@@ -5,7 +5,6 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     minlength: 10,
     maxlength: 500,
-    required: true,
   },
   type: {
     type: String,
@@ -14,128 +13,197 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     minlength: 5,
     maxlength: 200,
-    required: true,
   },
   synopsis: {
     type: String,
     minlength: 5,
     maxlength: 1000,
-    required: true,
   },
   tags: {
     type: Array,
     required: true,
     maxlength: 4,
   },
-  procedures: [
-    {
-      text: {
-        type: String,
-        minlength: 5,
-        maxlength: 10000,
-        required: true,
-      },
-      url: {
-        type: String,
-        minlength: 10,
-        maxlength: 500,
-        required: true,
-      },
-    }
-   ],
+  procedures: {
+    type: Array,
+    maxlength: 50,
+  },
   author: {
     type: String,
     minlength: 5,
     maxlength: 200,
-    required: true,
   },
   total_energy: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_carbohydrate: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_sugars: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_fiber: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_sodium: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_protein: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_fat: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   total_cholesterol: {
       type: Number,
       required: true,
-      max: 100000,
   },
   total_glycemic_load: {
       type: Number,
-      required: true,
-      max: 100000,
   },
   created: {
     type: String,
-    minlength: 16,
-    maxlength: 16,
-    required: true,
+    minlength: 19,
+    maxlength: 19,
   },
   edited: {
     type: String,
-    minlength: 16,
-    maxlength: 16,
-    required: true,
+    minlength: 19,
+    maxlength: 19,
   },
   ingredients: [
     {
-      ingredient_id: {
+      _id: {
         type: String,
         minlength: 3,
-        maxlength: 100,
-        required: true,
+        maxlength: 50,
       },
-      ingredient_equivalence: {
+      equivalence: {
         cup: {
           type: Number,
-          required:true,
-          max:1000,
         },
         spoon: {
           type: Number,
-          required:true,
-          max:10000,
         },
         piece: {
           type: Number,
-          required:true,
-          max:1000,
         },
         gram: {
           type: Number,
-          required:true,
-          max:100000,
         },
-      }
+      },
+      name: {
+        type: String,
+        minlength: 2,
+        maxlength: 50,
+      },
+      alias: {
+        type: Array,
+        maxlength: 50,
+      },
+      url: {
+        type: String,
+        minlength: 2,
+        maxlength: 200,
+      },
+      consistency: {
+        type: String,
+        minlength: 1,
+        maxlength: 1,
+      },
+      energy: {
+        type: Number,
+      },
+      total_carbohydrate: {
+        type: Number,
+      },
+      dietary_fiber: {
+        type: Number,
+      },
+      sugars: {
+        type: Number,
+      },
+      calcium: {
+        type: Number,
+      },
+      phosphorus: {
+        type: Number,
+      },
+      iron: {
+        type: Number,
+      },
+      sodium: {
+        type: Number,
+      },
+      potassium: {
+        type: Number,
+      },
+      magnesium: {
+        type: Number,
+      },
+      copper: {
+        type: Number,
+      },
+      zinc: {
+        type: Number,
+      },
+      manganese: {
+        type: Number,
+      },
+      selenium: {
+        type: Number,
+      },
+      lithium: {
+        type: Number,
+      },
+      vitA: {
+        type: Number,
+      },
+      carotene: {
+        type: Number,
+      },
+      bcarotene: {
+        type: Number,
+      },
+      vitB1: {
+        type: Number,
+      },
+      vitC: {
+        type: Number,
+      },
+      folicAc: {
+        type: Number,
+      },
+      vitD: {
+        type: Number,
+      },
+      vitE: {
+        type: Number,
+      },
+      vitK: {
+        type: Number,
+      },
+      protein: {
+        type: Number,
+      },
+      total_fat: {
+        type: Number,
+      },
+      saturated_fatty_acids: {
+        type: Number,
+      },
+      monounsaturated_fatty_acids: {
+        type: Number,
+      },
+      polyunsaturated_fatty_acids: {
+        type: Number,
+      },
+      cholesterol: {
+        type: Number,
+      },
+      glycemic_index: {
+        type: Number,
+      },
     },
    ],
 });
